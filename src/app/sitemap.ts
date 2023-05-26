209780fe0e40d2a545707901b57ffa609fb60f68
Diego Alvarado Otey <d.alvaradotey@gmail.com>
2023-05-25 22:57:00 -0400

    ADD: sitemap y robots

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next';
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://anyaeco.com',
+      lastModified: '2023-05-26T02:52:52.091Z',
+    },
+    {
+      url: 'https://anyaeco.com/tienda',
+      lastModified: '2023-05-26T02:52:52.091Z',
+    },
+    {
+      url: 'https://anyaeco.com/nosotros',
+      lastModified: '2023-05-26T02:52:52.091Z',
+    },
+    {
+      url: 'https://anyaeco.com/blog',
+      lastModified: '2023-05-26T02:52:52.091Z',
+    },
+    {
+      url: 'https://anyaeco.com/blog/compromiso-consumo-consciente',
+      lastModified: '2023-05-26T02:52:52.091Z',
+    },
+    {
+      url: 'https://anyaeco.com/blog/impacto-consumir-tendencia',
+      lastModified: '2023-05-26T02:52:52.091Z',
+    },
+  ];
+}
